6a5750300aa58874606a3fcd544d26896fbc6716
Tjnk <24>
2020-05-01 17:04:08 +0300

    removing iframe-riser

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { iframeResizerContentWindow } from 'iframe-resizer'; // eslint-disable-line no-unused-vars
 import style from './main.pcss'; // eslint-disable-line no-unused-vars
 
 window.addEventListener('load', () => document.getElementById('download-link').click());
